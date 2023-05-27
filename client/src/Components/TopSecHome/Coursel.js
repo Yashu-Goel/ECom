d4dd74f9eac354c6b7e232b7847e0fcf7cfe5d68
Allen Benny <allenbenny038@gmail.com>
2023-05-28 01:17:30 +0530

    main coursel done

--- client/src/Components/TopSecHome/Coursel.js
+++ client/src/Components/TopSecHome/Coursel.js
@@ -1,130 +1,148 @@
 import React, { useState, useEffect } from "react";
-// import { Carousel, initTE } from "tw-elements";
+import "./Coursel.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Coursel = () => {
-
   return (
-    <div
-      id="carouselExampleIndicators"
-      class="relative"
-      data-te-carousel-init
-      data-te-carousel-slide
-    >
-      <div
-        class="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
-        data-te-carousel-indicators
+    <div className="carousel-wrapper">
+      <Carousel
+        className="coursel"
+        infiniteLoop
+        useKeyboardArrows={true}
+        autoPlay="true"
+        interval="5000"
+        showThumbs={false}
+        showStatus={false}
       >
-        <button
-          type="button"
-          data-te-target="#carouselExampleIndicators"
-          data-te-slide-to="0"
-          data-te-carousel-active
-          class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-te-target="#carouselExampleIndicators"
-          data-te-slide-to="1"
-          class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-te-target="#carouselExampleIndicators"
-          data-te-slide-to="2"
-          class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-
-      <div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-        <div
-          class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-te-carousel-item
-          data-te-carousel-active
-        >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-            class="block w-full"
-            alt="Wild Landscape"
-          />
-        </div>
-        <div
-          class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-te-carousel-item
-        >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-            class="block w-full"
-            alt="Camera"
-          />
-        </div>
-        <div
-          class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-te-carousel-item
-        >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-            class="block w-full"
-            alt="Exotic Fruits"
-          />
-        </div>
-      </div>
-
-      <button
-        class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-te-target="#carouselExampleIndicators"
-        data-te-slide="prev"
-      >
-        <span class="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6"
+        <div>
+          <img src={require("./img_1/Fasion.jpg")} alt="helo" />
+          <p
+            style={{
+              position: "absolute",
+              left: "50%",
+              bottom: "50%",
+              color: "white",
+              transform: " translateX(-50%)",
+              fontSize: "3rem",
+              textTransform: "uppercase",
+              fontWeight: "Bold",
+              padding: " 0 10px",
+              backgroundColor: "black",
+              borderRadius: "10px",
+              border: "2px solid #fff",
+            }}
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
-        </span>
-        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Previous
-        </span>
-      </button>
-      <button
-        class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-te-target="#carouselExampleIndicators"
-        data-te-slide="next"
-      >
-        <span class="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6"
+            trending styleverse&#128090;
+          </p>
+        </div>
+
+        <div>
+          <img src={require("./img_1/Gadgets.jpg")} alt="eletronics" />
+          <p
+            style={{
+              position: "absolute",
+              left: "50%",
+              bottom: "50%",
+              color: "white",
+              transform: " translateX(-50%)",
+              fontSize: "3rem",
+              textTransform: "uppercase",
+              fontWeight: "Bold",
+              padding: " 0 10px",
+              backgroundColor: "black",
+              borderRadius: "10px",
+              border: "2px solid #fff",
+            }}
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </span>
-        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Next
-        </span>
-      </button>
+            Gadgets&#128241;
+          </p>
+        </div>
+        <div>
+          <img src={require("./img_1/Fitness.jpg")} alt="fitness" />
+          <p
+            style={{
+              position: "absolute",
+              left: "50%",
+              bottom: "50%",
+              color: "white",
+              transform: " translateX(-50%)",
+              fontSize: "3rem",
+              textTransform: "uppercase",
+              fontWeight: "Bold",
+              padding: " 0 10px",
+              backgroundColor: "black",
+              borderRadius: "10px",
+              border: "2px solid #fff",
+            }}
+          >
+            Health and Fitness&#127947;
+          </p>
+        </div>
+        <div>
+          <img src={require("./img_1/HomeDecor.jpg")} alt="homeDecor" />
+          <p
+            style={{
+              position: "absolute",
+              left: "50%",
+              bottom: "50%",
+              color: "white",
+              transform: " translateX(-50%)",
+              fontSize: "3rem",
+              textTransform: "uppercase",
+              fontWeight: "Bold",
+              padding: " 0 10px",
+              backgroundColor: "black",
+              borderRadius: "10px",
+              border: "2px solid #fff",
+            }}
+          >
+            Home Decor&#129716;
+          </p>
+        </div>
+        <div>
+          <img src={require("./img_1/Dogs.jpg")} alt="homeDecor" />
+          <p
+            style={{
+              position: "absolute",
+              left: "50%",
+              bottom: "50%",
+              color: "white",
+              transform: " translateX(-50%)",
+              fontSize: "3rem",
+              textTransform: "uppercase",
+              fontWeight: "Bold",
+              padding: " 0 10px",
+              backgroundColor: "black",
+              borderRadius: "10px",
+              border: "2px solid #fff",
+            }}
+          >
+            Pet Supplies&#128062;
+          </p>
+        </div>
+        <div>
+          <img src={require("./img_1/Gifts.jpg")} alt="homeDecor" />
+          <p
+            style={{
+              position: "absolute",
+              left: "50%",
+              bottom: "50%",
+              color: "white",
+              transform: " translateX(-50%)",
+              fontSize: "3rem",
+              textTransform: "uppercase",
+              fontWeight: "Bold",
+              padding: " 0 10px",
+              backgroundColor: "black",
+              borderRadius: "10px",
+              border: "2px solid #fff",
+            }}
+          >
+            Surprise Spheres &#127873;
+          </p>
+        </div>
+      </Carousel>
     </div>
   );
 };
