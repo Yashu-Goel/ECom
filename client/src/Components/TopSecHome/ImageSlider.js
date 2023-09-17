93c88562a17babe50dff997526eb763fa040deeb
Allen Benny <allenbenny038@gmail.com>
2023-09-18 04:02:52 +0530

    hmm

--- client/src/Components/TopSecHome/ImageSlider.js
+++ client/src/Components/TopSecHome/ImageSlider.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./ImageSlider.css";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
@@ -8,98 +9,92 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 const ImageSlider = ({ slider, loop, text }) => {
-  const imageList = document.querySelector(".custom-image-wrapper");
+  const [swiperConfig, setSwiperConfig] = useState({
+    slidesPerView: 5,
+    spaceBetween: 10,
+    width: 1200,
+  });
 
-  const clickHandler = (direction) => {
-    const scrollAmount = imageList.clientWidth * direction;
-    imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
-  };
-
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     const screenWidth = window.innerWidth;
+  //     if (screenWidth <= 768) {
+  //       setSwiperConfig({
+  //         slidesPerView: 60,
+  //         spaceBetween: 10,
+  //         width: 700,
+  //       });
+  //     } else if (screenWidth <= 992) {
+  //       setSwiperConfig({
+  //         slidesPerView: 3,
+  //         spaceBetween: 10,
+  //         width: 700,
+  //       });
+  //     } else if (screenWidth <= 1200) {
+  //       setSwiperConfig({
+  //         slidesPerView: 4,
+  //         spaceBetween: 10,
+  //         width: 900,
+  //       });
+  //     } else {
+  //       setSwiperConfig({
+  //         slidesPerView: 50,
+  //         spaceBetween: 10,
+  //         width: 1200,
+  //       });
+  //     }
+  //   };
+  //   window.onload = () => {
+  //     handleResize();
+  //     window.addEventListener("resize", handleResize);
+  //   };
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
+  // console.log(swiperConfig);
   return (
-    <div className="custom-image-slider">
-      <div className="custom-image-wrapper">
-        <button
-          className="custom-coursel-btn coursel-left"
-          onClick={() => clickHandler(-1)}
-        >
-          {"<"}
-        </button>
-        {/* <div className=""> */}
-        {slider?.map((item, index) => (
-          <div className="custom-image-card" key={index}>
-            <img
-              src={require(`./img_1/${item.pic}`)}
-              alt={item.pic}
-              className="card-img"
-            />
-            {/* <div className="text-content">
-              <div className="text-inner-content"></div>
-              <div className="text-inner-content"></div>
-              <div className="text-inner-content"></div>
-            </div> */}
-          </div>
-        ))}
-
-        <button
-          className="custom-coursel-btn coursel-right"
-          onClick={() => clickHandler(1)}
-        >
-          {">"}
-        </button>
-      </div>
-
-      <div className="custom-slider-scrollbar">
-        <div className="custom-scrollbar-track">
-          <div className="custom-scrollbar-thumb"></div>
-        </div>
-      </div>
-    </div>
-    // <Swiper
-    //   slidesPerView={6}
-    //   navigation
-    //   spaceBetween={10}
-    //   modules={[Autoplay, Pagination, Navigation]}
-    //   className="slide-container"
-    //   autoplay={loop && { delay: 2500 }}
-    // >
-    //   {slider.map((val, i) => {
-    //     return (
-    //       <SwiperSlide key={i}>
-    //         <div className="slide-content">
-    //           <div className="card-wrapper swiper-wrapper">
-    //             <Link
-    //               to={{
-    //                 pathname: `/product/${val._id}`,
-    //               }}
-    //             >
-    //               <div className="card swiper-slide">
-    //                 <div className="image-content">
-    //                   <div className="card-image">
-    //                     <img
-    //                       src={require(`./img_1/${val.pic}`)}
-    //                       alt={val.pic}
-    //                       className="card-img"
-    //                     />
-    //                   </div>
-    //                 </div>
-    //                 {text && (
-    //                   <div className="card-content">
-    //                     <div className="main-head">
-    //                       <span className="offer-tag">{val.offerTag}</span>
-    //                       &nbsp;
-    //                       <p className="deal-details">{val.dealDetails}</p>
-    //                     </div>
-    //                     <p className="item-name">{val.itemName}</p>
-    //                   </div>
-    //                 )}
-    //               </div>
-    //             </Link>
-    //           </div>
-    //         </div>
-    //       </SwiperSlide>
-    //     );
-    //   })}
-    // </Swiper>
+    <Swiper
+      {...swiperConfig}
+      navigation
+      modules={[Autoplay, Pagination, Navigation]}
+      className="slide-container"
+      autoplay={loop && { delay: 2500 }}
+    >
+      {slider.map((val, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="slide-content">
+              <div className="card-wrapper swiper-wrapper">
+                <Link
+                  to={{
+                    pathname: `/product/${val._id}`,
+                  }}
+                >
+                  <div className="card swiper-slide">
+                    <div className="card-image">
+                      <img
+                        src={require(`./img_1/${val.pic}`)}
+                        alt={val.pic}
+                        className="card-img"
+                      />
+                    </div>
+                    {text && (
+                      <div className="main-head">
+                        <p id="offer-tag">{val.offerTag}</p>
+                        &nbsp;
+                        <p id="deal-details">{val.dealDetails}</p>
+                        <p id="item-name">{val.itemName}</p>
+                      </div>
+                    )}
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
